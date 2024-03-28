c0a1d4b58ec89149d6169f7341ea025581bc8133
lucas <stempest@163.com>
2024-03-28 17:35:46 +0800

    home base ui finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ export function Home() {
     <div class="container mx-auto py-12 flex flex-row space-x-8 h-full overflow-y-scroll">
       {/* form */}
       <aside className="bg-white w-[360px] rounded-lg shadow-md p-6 flex flex-col space-y-6">
+        <h2 className="font-bold text-2xl text-slate-700">Search Form</h2>
         {/* input 1 */}
         <div>
           <label className="font-medium text-slate-600">Days</label>
@@ -55,7 +56,42 @@ export function Home() {
       </aside>
 
       {/* table */}
-      <div className="flex-1 bg-white rounded-lg shadow-md p-6"></div>
+      <div className="flex-1 bg-white rounded-lg shadow-md p-6">
+        <h2 className="font-bold text-2xl text-slate-700 mb-6">
+          Search Result
+        </h2>
+        <table className="border-collapse table-auto w-full">
+          <thead className="bg-slate-200">
+            <tr>
+              {[1, 2, 3, 4, 5].map((item) => (
+                <th
+                  key={item}
+                  className="border border-slate-300 font-bold text-left px-4 py-3 text-slate-600"
+                >
+                  Col {item}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {[1, 2, 3, 4, 5, 6].map((item) => (
+              <tr
+                key={item}
+                className="bg-white even:bg-slate-50 hover:bg-slate-100"
+              >
+                {[1, 2, 3, 4, 5].map((val) => (
+                  <td
+                    key={val}
+                    className="border text-left px-4 py-3 text-slate-500"
+                  >
+                    Value {val}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
